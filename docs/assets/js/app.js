1414264d4f74a73edabdfd5e61f8f4b6a26021e9
ABDELMAJID ELOUSSE <mjid.elouss@gmail.com>
2022-10-21 21:02:42 +0200

    delete finished

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -1,4 +1,4 @@
-// loaded tasks
+// Tasks Load variables
 const title = document.getElementById("title");
 const dateInput = document.getElementById("dateInput");
 const typeInput = document.getElementsByName("type");
@@ -10,18 +10,22 @@ const addTask = document.getElementById("to-do-tasks");
 const toDoTasks = document.getElementById("to-do-tasks");
 const inProgressTasks = document.getElementById("in-progress-tasks");
 const doneTasks = document.getElementById("done-tasks");
-const modelud = document.getElementById("delete-update-task");
+const modelFooter = document.getElementById("modalFooter");
 const toDoTaskCount = document.getElementById('to-do-tasks-count');
 const inProgressTaskCount = document.getElementById('in-progress-tasks-count');
 const doneTaskCount = document.getElementById('done-tasks-count');
-//
-
+// update & delete model variables
+const updateTitle = document.getElementById("update_title");
+const updateDate = document.getElementById("update_dateInput");
+const updateDesc = document.getElementById("update_desc");
+const updateStatus = document.getElementById("update_status");
+const updatePriority = document.getElementById("update_priority");
 
 let toDoCount = 0;
 let inProgressCount = 0;
 let doneCount = 0;
 taskLoad();
-
+console.log(tasks);
 document.addEventListener("DOMContentLoaded", () => {
     document
         .getElementById("save")
@@ -30,12 +34,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // document.getElementById("update").addEventListener("click", );
 
-let formValidation = () => {
+function formValidation(){
     if (title.value === "") {
         console.log("failure");
     } else {
         console.log("success");
-        acceptData();
+        addTasks();
         $('#modal-task').modal('hide')
         // Swal.fire(
         //   'Good job!',
@@ -43,16 +47,15 @@ let formValidation = () => {
         //   'success'
         //  )
     }
-};
+}
 var type_value;
-let acceptData = () => {
+function addTasks(){
     for (var i = 0; i < typeInput.length; i++) {
         if (typeInput[i].checked) {
             type_value = typeInput[i].value;
         }
     }
     let data = {
-        'id': (tasks.length) + 1,
         'title': title.value,
         'type': type_value,
         'priority': priority.value,
@@ -63,7 +66,7 @@ let acceptData = () => {
     tasks.push(data);
     taskLoad();
     resetTasks();
-};
+}
 
 function taskLoad() {
   toDoTasks.innerHTML = "";
@@ -79,14 +82,13 @@ function taskLoad() {
                 <button
                 data-bs-toggle="modal"
                 data-bs-target="#delete-update-task"
-                onclick="loadExistingTask(this.id)"
+                onclick="initializeTask(${i})"
                   class="col-12"
                   style="
                     background-color: #0f3460;
                     border: none;
                     border-bottom: 1px solid white;
                   "
-                  id="${tasks[i].id}"
                 >
                   <div class="">
                     <i class=""></i>
@@ -97,7 +99,7 @@ function taskLoad() {
                       >${tasks[i].title}
                     </div>
                     <div class="ms-4">
-                      <div class="text-white">#${tasks[i].id} created in ${tasks[i].date}</div>
+                      <div class="text-white">#${i+1} created in ${tasks[i].date}</div>
                       <div
                         class="text-white"
                         title=""
@@ -127,14 +129,13 @@ function taskLoad() {
               <button
               data-bs-toggle="modal"
               data-bs-target="#delete-update-task"
-              onclick="loadExistingTask(this.id)"
+              onclick="initializeTask(${i})"
                   class="col-12"
                   style="
                     background-color: #0f3460;
                     border: none;
                     border-bottom: 1px solid white;
                   "
-                  id="${tasks[i].id}"
                 >
                   <div class="">
                     <i class=""></i>
@@ -148,7 +149,7 @@ function taskLoad() {
                       >${tasks[i].title}
                     </div>
                     <div class="ms-4">
-                      <div class="text-white">#${tasks[i].id} created in ${tasks[i].date}</div>
+                      <div class="text-white">#${i+1} created in ${tasks[i].date}</div>
                       <div
                         class="text-white"
                         title=""
@@ -176,7 +177,7 @@ function taskLoad() {
             doneCount++;
             doneTasks.innerHTML += `
               <button
-              onclick="loadExistingTask(this.id)"
+              onclick="initializeTask(${i})"
               data-bs-toggle="modal"
               data-bs-target="#delete-update-task"
                   class="col-12"
@@ -185,7 +186,7 @@ function taskLoad() {
                     border: none;
                     border-bottom: 1px solid white;
                   "
-                  id="${tasks[i].id}"
+                  
                 >
                   <div class="">
                     <i class=""></i>
@@ -196,7 +197,7 @@ function taskLoad() {
                       >${tasks[i].title}
                     </div>
                     <div class="ms-4">
-                      <div class="text-white">#${tasks[i].id} created in ${tasks[i].date}</div>
+                      <div class="text-white">#${i+1} created in ${tasks[i].date}</div>
                       <div
                         class="text-white"
                         title=""
@@ -235,166 +236,65 @@ let resetTasks = () => {
     desc.value = "";
 };
 
-function loadExistingTask(index) {
-  for(t of tasks){
-    if(t.id == index){
-      if (t.type === "Feature") {
+function initializeTask(index) {
+      modelFooter.innerHTML = `<button
+      type="button"
+      style="
+        background-color: #c8c8c8;
+        color: #e94560;
+        font-weight: bold;
+      "
+      class="btn btn-light border rounded-pill"
+      data-bs-dismiss="modal"
+    >
+    Cancel
+    </button>
+      <button
+      type="button"
+      style="
+        background-color: #FF0660;
+        color: white;
+        font-weight: bold;
+      "
+      class="btn btn-light border rounded-pill"
+      onclick="deleteTask(${index+1})"
+    >
+    DELETE
+    </button>
+    <button
+      type="submit"
+      style="background-color: green; font-weight: bold"
+      class="btn btn-primary rounded-pill text-white"
+      onClick="editTask(${index})"
+    >
+    UPDATE
+    </button>`;
+    updateTitle.value = tasks[index].title;
+    if (tasks[index].type === "Feature") {
         document.getElementById("feature").checked = true;
         document.getElementById("bug").checked = false;
     } else {
         document.getElementById("feature").checked = false;
         document.getElementById("bug").checked = true;
     }
-    
-    }
-  }
-      modelud.innerHTML = `
-      <div class="modal-dialog">
-      <div class="modal-content">
-        <div
-          class="modal-header"
-          style="
-            background-image: linear-gradient(#e94560, #0f3460);
-            border: none;
-          "
-        >
-          <h5 class="modal-title text-white">Task</h5>
-        </div>
-        <div class="modal-body" style="background-color: #0f3460">
-          <div class="" id="taskForm">
-            <label class="col-form-label text-white">Title</label>
-            <input
-              type="text"
-              class="form-control"
-              style="background-color: #c8c8c8"
-              id="update_title"
-              value="${tasks[index - 1].title}"
-            />
-          </div>
-          <label class="col-form-label text-white" id="typ">Type</label>
-          <div class="d-flex">
-            <div class="form-check ms-3">
-              <input
-                name="type"
-                value="Bug"
-                class="form-check-input"
-                type="radio"
-                id="bug"
-              />
-              <label class="form-check-label text-white">Bug</label>
-            </div>
-            <div class="form-check ms-3">
-              <input
-                name="type"
-                value="Feature"
-                class="form-check-input"
-                type="radio"
-                id="feature"
-              />
-              <label class="form-check-label text-white">Feature</label>
-            </div>
-          </div>
-          <div class="">
-            <label class="col-form-label text-white">Priority</label>
-            <select
-              class="form-select"
-              style="background-color: #c8c8c8"
-              id="update_priority"
-              value="${tasks[index - 1].priority}"
-            >
-              <option disabled selected>Please select</option>
-              <option value="Low">Low</option>
-              <option value="Medium">Medium</option>
-              <option value="High">High</option>
-              <option value="Critical">Critical</option>
-            </select>
-          </div>
-          <div class="">
-            <label class="col-form-label text-white">Status</label>
-            <select
-              class="form-select"
-              style="background-color: #c8c8c8"
-              id="update_status"
-              value="${tasks[index - 1].status}"
-            >
-              <option disabled selected>Please select</option>
-              <option value="To Do">To Do</option>
-              <option value="In Progress">In Progress</option>
-              <option value="Done">Done</option>
-            </select>
-          </div>
-          <div class="">
-            <label class="col-form-label text-white">Date</label>
-            <input
-              type="date"
-              class="form-control"
-              style="background-color: #c8c8c8"
-              id="update_dateInput"
-              value="${tasks[index - 1].date}"
-            />
-          </div>
-          <div class="">
-            <label class="col-form-label text-white">Description</label>
-            <textarea
-              class="form-control"
-              style="background-color: #c8c8c8"
-              id="update_desc"
-            >${tasks[index - 1].description}</textarea>
-          </div>
-        </div>
-        <div
-          class="modal-footer"
-          style="background-color: #0f3460; border: none"
-        >
-          <button
-            type="button"
-            style="
-              background-color: #c8c8c8;
-              color: #e94560;
-              font-weight: bold;
-            "
-            class="btn btn-light border rounded-pill"
-            id = "${index}"
-            onclick="deleteTask(this.id)"
-          >
-          DELETE
-          </button>
-          <button
-            type="submit"
-            style="background-color: #e94560; font-weight: bold"
-            class="btn btn-primary rounded-pill text-white"
-            onClick="editTask(this.id)"
-            id = "${index}"
-          >
-          UPDATE
-          </button>
-        </div>
-      </div>
-      </div>
-      `;
+    updatePriority.value = tasks[index].priority;
+    updateStatus.value = tasks[index].status;
+    updateDate.value = tasks[index].date;
+    updateDesc.value = tasks[index].description;
 }
 
 function editTask(index){
-const updateTitle = document.getElementById("update_title");
-const updateDate = document.getElementById("update_dateInput");
-const updateDesc = document.getElementById("update_desc");
-const updateStatus = document.getElementById("update_status");
-const updatePriority = document.getElementById("update_priority");
   for (let j = 0; j < typeInput.length; j++) {
     if (typeInput[j].checked) {
         type_value = typeInput[j].value;
     }
 }
-for(t of tasks){
-  if(t.id == index){
-    t.title =  updateTitle.value,
-    t.type = type_value,
-    t.priority = updatePriority.value,
-    t.status = updateStatus.value,
-    t.date = updateDate.value,
-    t.description = updateDesc.value
-  }
-}
+  tasks[index].title =  updateTitle.value,
+  tasks[index].type = type_value,
+  tasks[index].priority = updatePriority.value,
+  tasks[index].status = updateStatus.value,
+  tasks[index].date = updateDate.value,
+  tasks[index].description = updateDesc.value
 $('#delete-update-task').modal('hide')
 taskLoad();
 }
@@ -405,46 +305,5 @@ function deleteTask(index) {
   console.log(tasks);
   $('#delete-update-task').modal('hide');
   taskLoad();
-}
-
-/**
- * In this file app.js you will find all CRUD functions name.
- * 
- */
-
-function createTask() {
-    // initialiser task form
-
-    // Afficher le boutton save
-
-    // Ouvrir modal form
-}
-
-function saveTask() {
-
-}
-
-function updateTask() {
-    // GET TASK ATTRIBUTES FROM INPUTS
-
-    // Créez task object
-
-    // Remplacer ancienne task par nouvelle task
-
-    // Fermer Modal form
-
-    // Refresh tasks
-
-}
-
-function initTaskForm() {
-    // Clear task form from data
-
-    // Hide all action buttons
-}
-
-function reloadTasks() {
-    // Remove tasks elements
-
-    // Set Task count
+  resetTasks();
 }
